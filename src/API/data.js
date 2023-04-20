5f4e9ca6de665f029b5b81f7e6ac85d7606a7837
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-04-20 10:42:40 +0300

    Add dot env file

--- src/API/data.js
+++ src/API/data.js
@@ -2,4 +2,3 @@ export const URL = 'https://financialmodelingprep.com/api/v3/';
 export const Sectors = 'sectors-performance';
 export const Companies = 'stock-screener';
 export const Company = 'profile/';
-export const APIKey = '34e63e51340d93d9d3a2c884a23c2310';
--- src/API/dataAPI.js
+++ src/API/dataAPI.js
@@ -1,13 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import {
-  URL, Company, Companies, APIKey, Sectors,
+  URL, Company, Companies, Sectors,
 } from './data';
 
 const fetchCompanies = createAsyncThunk(
   'companies/getCompanies',
   async (sector) => {
-    const baseUrl = `${URL}${Companies}?sector=${sector}&apikey=${APIKey} `;
+    const baseUrl = `${URL}${Companies}?sector=${sector}&apikey=${process.env.REACT_APP_FINANCE_API_KEY} `;
     const response = await axios.get(baseUrl);
     return response.data;
   },
@@ -15,7 +15,7 @@ const fetchCompanies = createAsyncThunk(
 const fetchCompany = createAsyncThunk(
   'company/fetchCompany',
   async (symbol) => {
-    const baseUrl = `${URL}${Company}${symbol}?apikey=${APIKey} `;
+    const baseUrl = `${URL}${Company}${symbol}?apikey=${process.env.REACT_APP_FINANCE_API_KEY}} `;
     const response = await axios.get(baseUrl);
     return response.data[0];
   },
@@ -23,7 +23,7 @@ const fetchCompany = createAsyncThunk(
 const fetchSectors = createAsyncThunk(
   'sectors/fetchSectors',
   async () => {
-    const baseUrl = `${URL}${Sectors}?apikey=${APIKey} `;
+    const baseUrl = `${URL}${Sectors}?apikey=${process.env.REACT_APP_FINANCE_API_KEY}} `;
     const response = await axios.get(baseUrl);
     return response.data;
   },
